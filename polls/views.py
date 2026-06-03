b0a22f551d81b22c16f529d6f399339dfc053045
HamSamm <samuel.cai@ssc-spc.gc.ca>
2026-06-03 14:20:10 -0400

    Save

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from .models import Question
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=questino_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
